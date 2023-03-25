c11c44fdb62deaf3da62a65a6c615bc91dad614d
Aleksander <110952267+Dmitri3v@users.noreply.github.com>
2023-03-24 19:20:06 -0500

    Create py_bot.py

---
+++ bot/py_bot.py
@@ -0,0 +1,22 @@
+from tel_config import *
+import telebot
+
+bot = telebot.TeleBot(tel_thonken)
+
+@bot.message_handler(commands=["start"])
+def cmd_start(message):
+    bot.reply_to(message, "Hi, im bot for Tao TV")
+
+
+@bot.message_handler(content_types=["text"])
+def bot_msg_txt(message):
+    if message.text.startswith("/"):
+        bot.send_message(message.chat.id, "Command not found")
+    else:
+        bot.send_message(message.chat.id, "Hola!")
+
+
+if __name__ == '__main__':
+    print("Bot is active")
+    bot.infinity_polling()
+    print("Bot disconnect")
