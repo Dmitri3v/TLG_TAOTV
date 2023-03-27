64ae75211639d41a674a9d8282395c7b87710aa0
Aleksander <110952267+Dmitri3v@users.noreply.github.com>
2023-03-27 12:39:19 -0500

    Add basic description code implementation.

--- bot/py_bot.py
+++ bot/py_bot.py
@@ -3,18 +3,22 @@ import telebot
 
 bot = telebot.TeleBot(tel_thonken)
 
+    ### Este apartade es para definir los comandos principales
 @bot.message_handler(commands=["start", "iniciar"])
 def cmd_start(message):
     bot.reply_to(message, "Hola, soy el bot de TAOTV, actualmente me encuentro en desarrollo, por ello no tengo muchas funciones hasta el momento, sin embargo pronto sere completamente oficial, ¡¡HASTA LA VICTORIA SIEMPRE!!")
 
+    ### En este apartado de define el comando "acerca" para que sea ejecutado
 @bot.message_handler(commands=["acerca"])
 def cmd_who(message):
     bot.reply_to(message, "Actualmente me esta desarrollando Kepishdo, si lo quieres contactar escribe el comando /contact y te enviare su correo")
 
+    ### En este apartado se define el comando contact
 @bot.message_handler(commands=["contact"])
 def cmd_contact(message):
     bot.reply_to(message, "Este es su correo: aleksander@duck.com , enviale saludos de mi parte!")
 
+    ### En este apartado se define lo que se va a responder en caso de enviar algun comando que no este definido aun, y que se le va a responder en caso de que envie solo texto.
 @bot.message_handler(content_types=["text"])
 def bot_msg_txt(message):
     if message.text.startswith("/"):
@@ -22,7 +26,7 @@ def bot_msg_txt(message):
     else:
         bot.send_message(message.chat.id, "Soy el bot de TAO TV!")
 
-
+    ### Este es un bucle para que el bot no pare de correr
 if __name__ == '__main__':
     print("Bot is active")
     bot.infinity_polling()
